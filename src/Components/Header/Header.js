55440f27dac055999ba8b19f8f80eea8b1e64fff
mdtohid <sagormdtohid@gmail.com>
2022-05-08 22:32:37 +0600

    first commit

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Navbar, Nav, Container} from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <div>
+        <Navbar collapseOnSelect sticky='top' expand="lg" bg="dark" variant="dark">
+        <Container >
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="#pricin">Pricin</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">More deets</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+        </Container>
+      </Navbar>
+        </div>
+    );
+};
+
+export default Header;
